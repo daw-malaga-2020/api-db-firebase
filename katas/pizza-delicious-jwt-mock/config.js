e4762bb1a2fdd81c003fa2baf15f8d2622a1c3c3
Álex Martín <alex@alexdw.com>
2020-04-27 09:34:36 +0200

    Added config for Heroku deployment

--- katas/pizza-delicious-jwt-mock/config.js
+++ katas/pizza-delicious-jwt-mock/config.js
@@ -1,11 +1,15 @@
 // config.js
-const dotenv = require('dotenv')
-const result = dotenv.config()
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv')
+  const result = dotenv.config()
 
-if (result.error) {
-  throw result.error
+  if (result.error) {
+    throw result.error
+  }
+
+  const envs = result.parsed
+
+  module.exports = envs
+}else{
+  module.exports = process.env
 }
-
-const envs = result.parsed
-
-module.exports = envs
